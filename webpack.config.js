85b098ee1da964123faacd02060f1d8bf6451df1
tsuchiya_daisuke <tsuchiya_daisuke@liginc.co.jp>
2018-07-02 12:42:54 +0900

    Clean contents in dist directory before output

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
   entry: './src/js/index.js',
+  plugins: [
+    new CleanWebpackPlugin([ 'dist' ])
+  ],
   output: {
     filename: 'main.js',
     path: `${__dirname}/dist/js`
